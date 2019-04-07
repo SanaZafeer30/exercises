7704c09ba1d9e4ec38f478b1d3b5c9689b3a5aab
SanaZafeer30 <47333854+SanaZafeer30@users.noreply.github.com>
2019-04-07 23:51:24 +0530

    Create App.js

---
+++ Cart/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import CartManager from './components/containers/CartManager/CartManager'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>CART</h1>
+
+      
+      <CartManager/>
+  
+      </div>
+    );
+  }
+}
+
+export default App;
