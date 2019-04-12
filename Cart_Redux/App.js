8120e4b9cd4132c451cb12071d23d0c4da819ea0
SanaZafeer30 <47333854+SanaZafeer30@users.noreply.github.com>
2019-04-12 14:03:13 +0530

    Update App.js

--- Cart_Redux/App.js
+++ Cart_Redux/App.js
@@ -1,1 +1,28 @@
 import React, { Component } from "react";
+import Items from  "./component/items";
+import "./App.css";
+import Cart from  "./component/cartview";
+
+class App extends Component {
+    add = () => {
+        this.props.add();
+    };
+
+    render() {
+        return (
+
+            <div className="App">
+                <header className="header">
+                    <h1>TTN MART</h1>
+                </header>
+                <section>
+                    <Items className="items" />
+
+                    <Cart className="cart" />
+                </section>
+            </div>
+
+        );
+    }
+}
+export default App;
